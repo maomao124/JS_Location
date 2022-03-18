12bca642f6fd26a691a1801cb4c2f7ce91ef7734
毛 <1296193245@qq.com>
2022-03-18 21:47:39 +0800

    JS_Location

---
+++ target/JS_Location-1.0-SNAPSHOT/test2.js
@@ -0,0 +1,22 @@
+/**
+ * Project name(项目名称)：JS_Location
+ * File name(文件名): test2
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/3/18
+ * Time(创建时间)： 21:42
+ * Version(版本): 1.0
+ * Description(描述)： 无
+ */
+
+// location 对象中的方法
+//
+// 方法	        说明
+// assign()	加载指定的 URL，即载入指定的文档。
+// reload()	重新加载当前 URL。
+// replace()	用指定 URL 替换当前的文档，与 assign() 方法不同的是，使用 replace() 替换的新页面不会保存在浏览历史中，用户不能使用后退来返回该页面。
+// toString()	与 href 属性的效果相同，以字符串的形式返回当前完整的 URL。
+
+var url = window.location;
+document.write("<br>");
